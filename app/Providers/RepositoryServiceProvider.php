19cb19a89e621077cf8f175629705f48ef101b0c
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2022-11-30 19:48:14 -0300

    feat: add bind repository contract and class

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\BaseRepository;
+use App\Repositories\Contracts\BaseRepositoryContract;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(BaseRepositoryContract::class, BaseRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
