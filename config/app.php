49ef1ef2841cc21822e6af0be1b76175cf54c817
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2022-11-30 19:50:08 -0300

    feat: add new provider

--- config/app.php
+++ config/app.php
@@ -195,6 +195,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Project Providers
+         */
+        App\Providers\RepositoryServiceProvider::class,
+
     ],
 
     /*
