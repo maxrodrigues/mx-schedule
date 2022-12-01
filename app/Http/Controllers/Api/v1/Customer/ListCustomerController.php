0451709e8f99aba155aa2012d1bc03d04a205cb1
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2022-12-01 19:55:57 -0300

    feat: add list customers

--- app/Http/Controllers/Api/v1/Customer/ListCustomerController.php
+++ app/Http/Controllers/Api/v1/Customer/ListCustomerController.php
@@ -3,22 +3,33 @@
 namespace App\Http\Controllers\Api\v1\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\CustomerRepositoryContract;
+use App\Services\Contracts\CustomerServiceContract;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ListCustomerController extends Controller
 {
-    public function __construct()
-    {
+    /**
+     * customerService
+     *
+     * @var mixed
+     */
+    private $customerService;
 
+    public function __construct(CustomerServiceContract $customerService)
+    {
+        $this->customerService = $customerService;
     }
 
     public function __invoke(Request $request): JsonResponse
     {
         try {
+            $all = $this->customerService->listAllCustomers();
+
             return new JsonResponse([
-                'message' => $request->all()
+                'message' => $all
             ], Response::HTTP_OK);
         } catch (\Exception $e) {
 
