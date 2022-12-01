5469098073009322401a3be2d3004b827290ef2c
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2022-12-01 20:14:08 -0300

    feat: add new functions and comments

--- app/Services/Contracts/CustomerServiceContract.php
+++ app/Services/Contracts/CustomerServiceContract.php
@@ -3,8 +3,23 @@
 namespace App\Services\Contracts;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 interface CustomerServiceContract
 {
+    /**
+     * Get all customers.
+     *
+     * @return Collection | null
+     */
     public function listAllCustomers(): ?Collection;
+
+    /**
+     * Create new customer.
+     *
+     * @param  mixed $payload
+     * @return Model | null
+     */
+    public function createNewCustomer(array $payload): ?Model;
+
 }
--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -5,20 +5,48 @@ namespace App\Services;
 use App\Repositories\Contracts\CustomerRepositoryContract;
 use App\Services\Contracts\CustomerServiceContract;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class CustomerService implements CustomerServiceContract {
 
+    /**
+     * @var CustomerRepositoryContract
+     */
     private $customerRepository;
 
+    /**
+     * __construct
+     *
+     * @param  CustomerRepositoryContract $customerRepository
+     * @return void
+     */
     public function __construct(CustomerRepositoryContract $customerRepository)
     {
         $this->customerRepository = $customerRepository;
     }
 
+    /**
+     * Get all customers.
+     *
+     * @return Collection | null
+     */
     public function listAllCustomers(): ?Collection
     {
         $all = $this->customerRepository->all();
 
         return $all;
+    }
+
+    /**
+     * Create new customer
+     *
+     * @param  array $payload
+     * @return Model | null
+     */
+    public function createNewCustomer(array $payload): ?Model
+    {
+        $customer = $this->customerRepository->create($payload);
+
+        return $customer;
     }
 }
