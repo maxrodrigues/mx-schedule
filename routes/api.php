a74823b0b119bc160afaee3a6e8457c7ce8a68ad
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2022-12-01 20:40:13 -0300

    feat: add new route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\v1\Auth\RegisterController;
 use App\Http\Controllers\Api\v1\Customer\CreateCustomerController;
 use App\Http\Controllers\Api\v1\Customer\ListCustomerController;
 use App\Http\Controllers\Api\v1\Service\CreateServiceController;
+use App\Http\Controllers\Api\v1\Service\ListServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,7 +27,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
 
-Route::post('service/create', CreateServiceController::class);
-
+// CUSTOMERS
 Route::get('customer/', ListCustomerController::class);
 Route::post('customer/create', CreateCustomerController::class);
+
+// SERVICES
+Route::get('services/', ListServiceController::class);
+Route::post('service/create', CreateServiceController::class);
