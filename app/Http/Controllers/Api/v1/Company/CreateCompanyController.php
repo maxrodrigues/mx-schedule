ce8ece4a2496a6af3a3ec33586a8c70c721dd6ea
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2022-12-19 18:51:59 -0300

    feat: add create company controller

---
+++ app/Http/Controllers/Api/v1/Company/CreateCompanyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Company;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreateCompanyController extends Controller
+{
+    public function __invoke()
+    {
+        //
+    }
+}
